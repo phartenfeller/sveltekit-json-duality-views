4e8f23c4411d1f4d8c2166f39b39b12c36f2ce2d
phartenfeller <pharti@hotmail.de>
2024-08-02 11:04:41 +0200

    kscope talk updates

--- db/1/1-all_movies_jdv.sql
+++ db/1/1-all_movies_jdv.sql
@@ -4,6 +4,13 @@ create or replace json relational duality view all_movies_jdv as
     _id: movie_id,
     title,
     year,
-    rating: imdb_rating
+    rating: imdb_rating,
+    comments: movie_comments @insert
+    [
+      {
+        commentId: comment_id,
+        comment: comment_text
+      }
+    ]
   }
 ;
